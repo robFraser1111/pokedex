4fa29d514bfe013944c6fa83b738e99d5313fe9a
robFraser1111 <robfraserdesign@gmail.com>
2022-01-26 12:10:57 +1100

    Changed to HashRouter for deployment.

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
